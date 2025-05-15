ad34bca77f5d7c6aed125cfc7b40fbe86678d288
mazeus <mateusfrazao12345@gmail.com>
2025-05-15 14:39:14 -0300

    testando funcionamentos do style binding

--- src/app/components/binding/interpolation/interpolation.component.ts
+++ src/app/components/binding/interpolation/interpolation.component.ts
@@ -4,10 +4,49 @@ import { Component } from '@angular/core';
   selector: 'app-interpolation',
   imports: [],
   templateUrl: './interpolation.component.html',
-  styleUrl: './interpolation.component.css'
+  styleUrl: './interpolation.component.css',
 })
 export class InterpolationComponent {
-     titulo = 'Bem Vindo ao Angular'
-     desc = 'Aprendendo Angular com Interpolação'
-     texto = 'Texto de exemplo para interpolação'
+  titulo = 'Bem Vindo ao Angular';
+  desc = 'Aprendendo Angular com Interpolação';
+  texto = 'Texto de exemplo para interpolação';
+  status = 'inativo';
+  corStatus = 'red';
+  corBotao = 'white';
+  textoBotao = 'Ativar';
+  tamanhoBTN = 200;
+  larguraBTN = 200;
+
+  alternarStatus() {
+    if (this.status === 'ativo') {
+      this.status = 'inativo';
+      this.corStatus = 'red';
+      this.corBotao = 'white';
+      this.textoBotao = 'Ativar';
+    } else {
+      this.status = 'ativo';
+      this.corStatus = 'green';
+      this.corBotao = 'green';
+      this.textoBotao = 'Desativar';
+    }
+  }
+
+  aumentarTamanhoBTN() {
+    this.tamanhoBTN += 30;
+    console.log('Tamanho do botão aumentado para: ' + this.tamanhoBTN);
+  }
+
+  diminuirTamanhoBTN() {
+    this.tamanhoBTN -= 30;
+  }
+
+  aumentarLargura() {
+    this.larguraBTN += 30;
+    console.log('Largura do botão aumentada para: ' + this.larguraBTN);
+  }
+
+  diminuirLargura() {
+    this.larguraBTN -= 30;
+    console.log('Largura do botão diminuída para: ' + this.larguraBTN);
+  }
 }
